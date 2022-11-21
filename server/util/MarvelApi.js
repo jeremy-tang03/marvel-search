d8c51db47c64bcebf749eb4af87fa2d6be114ee5
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-21 15:35:15 -0500

    complete marvel api fetch

--- server/util/MarvelApi.js
+++ server/util/MarvelApi.js
@@ -3,14 +3,26 @@ import { config } from "dotenv";
 const PRIVATE_KEY = config.env.MARVEL_PRIVATE_KEY;
 const PUBLIC_KEY = config.env.MARVEL_PUBLIC_KEY;
 
-function fetchMarvelDAta() {
+async function fetchMarvelData() {
 
     // you need a new ts every request                                                                                    
     let ts = new Date().getTime();
     let hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
 
-    const marvelURL = "https://gateway.marvel.com/v1/public/";
+
+    // todo install coors??
+    const marvelURL = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`;
+    try {
+        const data = await fetch(marvelURL);
+        const jsonData = await data.json()
+        return jsonData;
+    }
+
+    catch(err) {
+        console.error("Error fetching marvel api " + err)
+        throw err;
+    }
 
 };
 
-getMarvelResponse();
+module.exports = fetchMarvelData;
