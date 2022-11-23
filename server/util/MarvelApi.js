76b8cf92584536d668e2d8d85fe99671e9b580f8
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-23 16:44:12 -0500

    Add method to fetch all marvel characters

--- server/util/MarvelApi.js
+++ server/util/MarvelApi.js
@@ -1,29 +1,54 @@
-import { config } from "dotenv";
+const Config = require('./config.js');
+const MD5 = require('crypto-js/md5.js')
 
-// add your api keys to config.env
-const PRIVATE_KEY = config.env.MARVEL_PRIVATE_KEY;
-const PUBLIC_KEY = config.env.MARVEL_PUBLIC_KEY;
-
-async function fetchMarvelData() {
-
-    // ts, used to create different hash every time                                                                               
-    let ts = new Date().getTime();
-    let hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
-
-
-    // todo install coors??
-    const marvelURL = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}`;
-    try {
-        const data = await fetch(marvelURL);
-        const jsonData = await data.json()
-        return jsonData;
+async function getJSON(url) {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error('ERROR: (${res.status})');
     }
+    return data;
+  }
+  
+  async function getCharacters(offset) {
+    const url = new URL(Config.API_URL + Config.CHARACTERS_PATH);
+    const { ts, hash } = generateAuthParams();
+    url.search = new URLSearchParams({
+      offset,
+      limit: 100,
+      ts,
+      hash,
+      apikey: Config.PUBLIC_KEY,
+    });
+    const { data } = await getJSON(url);
+    const { results } = data;
+    return results;
+  }
+  
+  async function getAllCharacterData() {
+    const characters = [];
+    const dataSets = await Promise.all(
+      [...Array(Config.NUM_OF_OFFSETS)].map((_, index) => getCharacters(index * 100))
+    );
+    dataSets.forEach((data) => {
+      const charData = [...data].map(({ id, name, thumbnail, comics }) => {
+        return { id, name, thumbnail, comics };
+      });
+      characters.push(...charData);
+    });
+    return characters.filter(char => char.comics.available > 0);
+  }
+  
+  function generateAuthParams() {
+    const ts = Date.now();
+    const hash = MD5(ts + Config.PRIVATE_KEY + Config.PUBLIC_KEY).toString();
+    return { ts, hash };
+  }
+  
 
-    catch(err) {
-        console.error("Error fetching marvel api " + err)
-        throw err;
-    }
-
-};
-
-module.exports = fetchMarvelData;
+module.exports = getAllCharacterData;
