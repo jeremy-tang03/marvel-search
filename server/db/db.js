e902c3bba5fab0702a64e282e76e919eeea56357
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-11-23 17:02:55 -0500

    create db singleton

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,40 @@
-import { config } from "dotenv";
-
 require("dotenv").config();
 
 const dbURL = config.env.ATLAS_URI;
 const { MongoCLient } = require("mongodb");
+let instance;
 
+class db {
+    constructor() {
+        if (!instance) {
+            instance = this
+            this.client = new MongoCLient(dbURL);
+            this.db = null;
+            this.collection = null;
+        }
+        return instance;
+    }
+    
+    // connect to db
+    async connect(dbName, collName) {
+        // if instance db is not null
+        if (instance.db) {
+            return;
+        }
+    
+        await instance.client.connect();
+        instance.db = await instance.client.db(dbName);
+        console.log("Connected to MongoDB database " + dbName);
+        instance.collection = await instance.db.collection(collName)
+    }
+    
+    // close db
+    async close() {
+        await instance.client.close();
+        instance = null;
+    }
+    
+    // TODO return character names
+    
+    // TODO return character data
+}
