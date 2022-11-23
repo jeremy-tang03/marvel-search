7d8b6b0b47b3893593b8fecf9abe8fe48f9ea5a1
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-23 16:30:07 -0500

    Add config file

---
+++ server/util/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const MD5 = require('crypto-js/md5.js')
+
+const API_URL = 'https://gateway.marvel.com/';
+const CHARACTERS_PATH = 'v1/public/characters';
+const COMICS_PATH = 'v1/public/comics';
+const NUM_OF_OFFSETS = 16;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+
+module.exports = {
+  API_URL,
+  CHARACTERS_PATH,
+  COMICS_PATH,
+  NUM_OF_OFFSETS,
+  PRIVATE_KEY,
+  PUBLIC_KEY
+}
