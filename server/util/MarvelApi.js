a2d865cbb939a495ea231274a40f9e6701b188e8
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-11-26 19:06:44 -0500

    Add method to get available series for a character

--- server/util/MarvelApi.js
+++ server/util/MarvelApi.js
@@ -52,19 +52,25 @@ async function getAllCharacterData() {
   const characters = [];
 
   const dataSets = await Promise.all(
-    [...Array(Config.NUM_OF_OFFSETS_COMICS)].map((_, index) =>
-      getCharacters(index * 100)
-    )
+    [...Array(1)].map((_, index) => getCharacters(index * 100))
   );
   dataSets.forEach((data) => {
-    const charData = [...data].map(({ id, name, thumbnail, series }) => {
-      return { id, name, thumbnail, series };
+    const charData = [...data].map(async ({ id, name, thumbnail, series }) => {
+      return {
+        id,
+        name,
+        thumbnail,
+        allSeries: await getAvailableSeries(series),
+      };
     });
     characters.push(...charData);
   });
 
-  // Filter out characters that have no comics
-  return characters.filter((char) => char.series.available > 0);
+  
+  const returnedChars = await Promise.all(characters)
+
+  // Filter out characters that have no series
+  return returnedChars.filter(char => char.allSeries.length > 0);
 }
 
 // Returns an array of all marvel series from the API
@@ -84,6 +90,23 @@ async function getAllSeriesData() {
   });
 
   return series;
+}
+
+// Gets all available series for a character
+async function getAvailableSeries(series) {
+  const url = new URL(series.collectionURI);
+  const { ts, hash } = generateAuthParams();
+  url.search = new URLSearchParams({
+    ts,
+    hash,
+    limit: Config.RESULT_LIMIT,
+    apikey: Config.PUBLIC_KEY,
+  });
+
+  const { data } = await getJSON(url);
+  return data.results.map(({ id, title }) => {
+    return { id, title };
+  });
 }
 
 // Generates a timestamp and hash
