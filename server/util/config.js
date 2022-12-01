63f07c6abe753a0a4e2cbfd5f0d265c5bbd8e5fe
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-01 14:46:17 -0500

    Remove unnecessary constants

--- server/util/config.js
+++ server/util/config.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 
 const API_URL = 'https://gateway.marvel.com/';
 const CHARACTERS_PATH = 'v1/public/characters';
-const SERIES_PATH = 'v1/public/series';
 const NUM_OF_OFFSETS_CHARS = 16;
-const NUM_OF_OFFSETS_SERIES = 133;
 const RESULT_LIMIT = 100;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
@@ -12,9 +10,7 @@ const PUBLIC_KEY = process.env.PUBLIC_KEY;
 module.exports = {
   API_URL,
   CHARACTERS_PATH,
-  SERIES_PATH,
   NUM_OF_OFFSETS_CHARS,
-  NUM_OF_OFFSETS_SERIES,
   RESULT_LIMIT,
   PRIVATE_KEY,
   PUBLIC_KEY
