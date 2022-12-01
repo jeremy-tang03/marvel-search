241762e63aa7a12c5f5716a91147c75ab055f493
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-01 14:47:21 -0500

    Modify getAllCharacterData method to fetch required information only

--- server/util/MarvelApi.js
+++ server/util/MarvelApi.js
@@ -31,83 +31,45 @@ async function getCharacters(offset) {
   return results;
 }
 
-// Gets an array of series
-async function getSeries(offset) {
-  const url = new URL(Config.API_URL + Config.SERIES_PATH);
-  const { ts, hash } = generateAuthParams();
-  url.search = new URLSearchParams({
-    offset,
-    limit: Config.RESULT_LIMIT,
-    ts,
-    hash,
-    apikey: Config.PUBLIC_KEY,
-  });
-  const { data } = await getJSON(url);
-  const { results } = data;
-  return results;
-}
-
 // Returns an array of all marvel characters from the API
 async function getAllCharacterData() {
   const characters = [];
 
   const dataSets = await Promise.all(
-    [...Array(Config.NUM_OF_OFFSETS_CHARS)].map((_, index) => getCharacters(index * 100))
-  );
-  dataSets.forEach((data) => {
-    const charData = [...data].map(async ({ id, name, thumbnail, series }) => {
-      return {
-        id,
-        name,
-        thumbnail,
-        allSeries: await getAvailableSeries(series),
-      };
-    });
-    characters.push(...charData);
-  });
-
-  
-  const returnedChars = await Promise.all(characters)
-
-  // Filter out characters that have no series
-  return returnedChars.filter(char => char.allSeries.length > 0);
-}
-
-// Returns an array of all marvel series from the API
-async function getAllSeriesData() {
-  const series = [];
-
-  const dataSets = await Promise.all(
-    [...Array(Config.NUM_OF_OFFSETS_SERIES)].map((_, index) =>
-      getSeries(index * 100)
+    [...Array(Config.NUM_OF_OFFSETS_CHARS)].map((_, index) =>
+      getCharacters(index * 100)
     )
   );
   dataSets.forEach((data) => {
-    const charData = [...data].map(({ id, title, startYear, endYear }) => {
-      return { id, title, startYear, endYear };
-    });
-    series.push(...charData);
+    const charData = [...data].map(
+      ({ id, name, thumbnail, comics, series, stories, events }) => {
+        const { available: numComics } = comics;
+        const { available: numSeries } = series;
+        const { available: numStories } = stories;
+        const { available: numEvents } = events;
+        return {
+          id,
+          name,
+          thumbnail,
+          numComics,
+          numSeries,
+          numStories,
+          numEvents,
+        };
+      }
+    );
+    characters.push(...charData);
   });
 
-  return series;
+  const returnedChars = await Promise.all(characters);
+
+  return returnedChars;
 }
 
-// Gets all available series for a character
-async function getAvailableSeries(series) {
-  const url = new URL(series.collectionURI);
-  const { ts, hash } = generateAuthParams();
-  url.search = new URLSearchParams({
-    ts,
-    hash,
-    limit: Config.RESULT_LIMIT,
-    apikey: Config.PUBLIC_KEY,
-  });
-
-  const { data } = await getJSON(url);
-  return data.results.map(({ id, title }) => {
-    return { id, title };
-  });
-}
+(async () => {
+  const data = await getAllCharacterData();
+  console.log(data)
+})();
 
 // Generates a timestamp and hash
 function generateAuthParams() {
@@ -116,4 +78,4 @@ function generateAuthParams() {
   return { ts, hash };
 }
 
-module.exports = {getAllCharacterData, getAllSeriesData};
+module.exports = getAllCharacterData;
