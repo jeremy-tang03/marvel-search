58cb7b708d92f8e9058a30308cc00dafcbd20f2b
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-12-07 15:50:20 -0500

    add 2 endpoints

--- server/routes/api.cjs
+++ server/routes/api.cjs
@@ -2,7 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Use \"/get/{id}\" or \"/search/{query}\"");
+})
+router.get("/get/:id", (req, res) => {
+    if('id' in req.params) {
+        res.json({'id' : req.params['id']})
+    } else {
+        res.status(400).json({error: 'not supported'});
+    }
+});
+router.get("/search/:query", (req, res) => {
+    if('query' in req.params) {
+        res.json({'query': req.params['query']});
+    } else {
+        res.status(400).json({error: 'not supported'});
+    }
 })
 
 // TODO add endpoints for client side
