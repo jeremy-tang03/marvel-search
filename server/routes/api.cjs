b30d794a9a4437512f1494d373e9d3707c47b8c5
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-12-07 16:46:56 -0500

    add db instance to api -Matthew and Jeremy

--- server/routes/api.cjs
+++ server/routes/api.cjs
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const DB = require("../db/db.js");
+
+const db = new DB();
+await db.connect("marvel", "characters");
 
 router.get("/", (req, res) => {
-    res.send("Use \"/get/{id}\" or \"/search/{query}\"");
+  res.send("Use \"/get/{id}\" or \"/search/{query}\"");
 })
 router.get("/get/:id", (req, res) => {
-    if('id' in req.params) {
-        res.json({'id' : req.params['id']})
-    } else {
-        res.status(400).json({error: 'not supported'});
-    }
+  if('id' in req.params) {
+    res.json({'id' : req.params['id']});
+  } else {
+    res.status(400).json({error: 'not supported'});
+  }
 });
-router.get("/search/:query", (req, res) => {
-    if('query' in req.params) {
-        res.json({'query': req.params['query']});
-    } else {
-        res.status(400).json({error: 'not supported'});
-    }
+router.get("/search/:query", async (req, res) => {
+  if('query' in req.params) {
+    console.log(await db.queryAllContaining(req.params.query));
+    res.json({'query': req.params['query']});
+  } else {
+    res.status(400).json({error: 'not supported'});
+  }
 })
 
 module.exports = router;
