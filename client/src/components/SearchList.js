284a662b74ee70e30879e552386bf0963b55b42a
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-09 11:58:28 -0500

    Add onclick prop

--- client/src/components/SearchList.js
+++ client/src/components/SearchList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card from './Card';
 
-function SearchList({ searchedCharacters }) {
-  const searchedChars = searchedCharacters.map(char =>  <Card  id={char.id} name={char.name} thumbnail={char.thumbnail}/>); 
-  return (
-    <div>
-      {searchedChars}
-    </div>
-  );
+function SearchList({ searchedCharacters, onCharClick }) {
+  const searchedChars = searchedCharacters.map((char) => (
+    <Card
+      id={char.id}
+      name={char.name}
+      thumbnail={char.thumbnail}
+      onCharacterClick={onCharClick}
+    />
+  ));
+  return <div>{searchedChars}</div>;
 }
 
 export default SearchList;
