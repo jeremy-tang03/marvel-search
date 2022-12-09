c2deebd729a968ee093e6e9d9c10bef35e9748c1
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-09 12:34:51 -0500

    Give class to element

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Card({ id, name, thumbnail, onCharacterClick }) {
   const imageUrl = `${thumbnail.path}.${thumbnail.extension}`;
   return (
-    <div className="tc bg-white dib br3 pa1 ma2 grow bw2 shadow-5" data-id={id} onClick={onCharacterClick}>
+    <div className="character-card tc bg-white dib br3 pa1 ma2 grow bw2 shadow-5" data-id={id} onClick={onCharacterClick}>
       <img className="br3 dib"src={imageUrl} alt={`${name}`} width="200px" height="200px" />
       <p>{name}</p>
     </div>
