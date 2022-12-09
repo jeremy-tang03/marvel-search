ab26b14bc5f1ed4f1f43126b909277e3e03ad571
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-09 11:57:56 -0500

    Add onlick prop

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Card({ id, name, thumbnail }) {
+function Card({ id, name, thumbnail, onCharacterClick }) {
   const imageUrl = `${thumbnail.path}.${thumbnail.extension}`;
   return (
-    <div className="tc bg-white dib br3 pa1 ma2 grow bw2 shadow-5" data-id={id}>
+    <div className="tc bg-white dib br3 pa1 ma2 grow bw2 shadow-5" data-id={id} onClick={onCharacterClick}>
       <img className="br3 dib"src={imageUrl} alt={`${name}`} width="200px" height="200px" />
       <p>{name}</p>
     </div>
