16f91efe54cd29c2d881f7ebc87bf34e5f138d55
Paul Patrick Rabanal <paul7098@hotmail.com>
2022-12-10 09:57:47 -0500

    Fix path to react build

--- server/app.cjs
+++ server/app.cjs
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const api = require("./routes/api.cjs");
 
-app.use(express.static("../client/public"));
+app.use(express.static("../client/build"));
 app.use("/api", api);
 app.use((req, res) => {
   res.status(404).json({error: "not supported"});
