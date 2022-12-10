d8a9d7ce46f25036f20ca9f01060985f8ed1627f
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2022-12-10 18:10:06 -0500

    add browser caching code

--- server/app.cjs
+++ server/app.cjs
@@ -3,8 +3,14 @@ const app = express();
 const api = require("./routes/api.cjs");
 const compression = require("compression");
 
+// set compression middleware
 app.use(compression());
 app.use(express.static("../client/build"));
+
+// set browser caching middleware
+app.use((req, res, next) => {
+  res.set("Cache-control", "public, max-age=31536000")
+})
 app.use("/api", api);
 app.use((req, res) => {
   res.status(404).json({error: "not supported"});
